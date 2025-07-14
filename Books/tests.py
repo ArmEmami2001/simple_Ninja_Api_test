3048c407825abcaf5e5ea69c4a218f806d678efd
Armin Emami <armemm2001@gmail.com>
2025-07-14 15:09:00 +0330

    test working

--- Books/tests.py
+++ Books/tests.py
@@ -1,30 +1,40 @@
-from django.urls import reverse
-from .models import Book
-from Library.api import api
+# Books/tests.py
+
+import json
 from django.test import TestCase, Client
+from .models import Book
 
 class BookTests(TestCase):
     def setUp(self):
-        self.book = Book.objects.create(title="Test Book", author="Test Author")
+        
         self.client = Client()
+        self.book = Book.objects.create(title="Test Book", author="Test Author")
 
     def test_book_creation(self):
-        response = self.client.post('/api/books/', {
-            'title': 'New Book',
-            'author': 'New Author'
-        })
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.json()['title'], 'New Book')
-        self.assertEqual(response.json()['author'], 'New Author')
+
+        payload = {'title': 'New Book', 'author': 'New Author'}
+        response = self.client.post(
+            '/api/books', 
+            data=json.dumps(payload), 
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, 200)
+        response_data = response.json()
+        self.assertEqual(response_data['title'], 'New Book')
+        self.assertEqual(response_data['author'], 'New Author')
 
     def test_book_list_view(self):
-        response = self.client.get('/api/books/')
+        
+        response = self.client.get('/api/books') 
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Test Book")
         self.assertContains(response, "Test Author")
 
     def test_book_detail_view(self):
-        response = self.client.get('/api/books/{}/'.format(self.book.id))
+       
+        url = f'/api/books/{self.book.id}'
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()['title'], self.book.title)
-        self.assertEqual(response.json()['author'], self.book.author)
+        response_data = response.json()
+        self.assertEqual(response_data['title'], self.book.title)
+        self.assertEqual(response_data['author'], self.book.author)
